443725aee8be4ed19e0f730cf7cb507a2bab0834
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-04-14 17:03:50 +0200

    Implement recursive Fib function.

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,22 +1,37 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let recursiveFibSeq n = 
+    let rec fibOperation n =
+        if n = 0 || n = 1 then
+            n
+        else
+            fibOperation (n - 2) + fibOperation (n - 1)
 
-let fibOperation (grandParent, parent) items =
-    (parent, grandParent + parent)
+    Seq.init n fibOperation
 
-let calcFibSeq n =
+
+let iterativeFibSeq n =
+    let fibOperation (grandParent, parent) items =
+        if grandParent = 0 && parent = 0 then // To have f(0)=0 this is how we make the induction-case
+            (0, 1)
+        else
+            (parent, grandParent + parent)
+
     Seq.init n id 
-    |> Seq.scan (fibOperation) (0, 1) 
+    |> Seq.scan fibOperation (0, 0)
     |> Seq.map snd
 
-let printFibSeq n =
-    let fibNumbers = calcFibSeq n
-    printf "First %i numbers are: \n" n
+let printFibSeq fibFunc n =
+    let fibNumbers = fibFunc n
     Seq.iter (fun i -> printf "%i " i) fibNumbers 
 
 [<EntryPoint>]
 let main argv =
-    printFibSeq 20
+    let fibFactor = 20
+    printf "First %i numbers by the recursive function are: \n" fibFactor
+    printFibSeq recursiveFibSeq fibFactor
+    printf "\n"
+    printf "First %i numbers by the iterative function are: \n" fibFactor
+    printFibSeq recursiveFibSeq fibFactor
     Console.ReadKey() |> ignore
     0 // return an integer exit code
