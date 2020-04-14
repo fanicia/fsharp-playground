c4871d0e5504d7b5ec090eaaf31577be1bd3a397
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-04-14 15:43:38 +0200

    Refactor into print function.

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -5,16 +5,18 @@ open System
 let fibOperation (grandParent, parent) items =
     (parent, grandParent + parent)
 
-let calcFibNumber n =
+let calcFibSeq n =
     Seq.init n id 
     |> Seq.scan (fibOperation) (0, 1) 
     |> Seq.map snd
 
+let printFibSeq n =
+    let fibNumbers = calcFibSeq n
+    printf "First %i numbers are: \n" n
+    Seq.iter (fun i -> printf "%i " i) fibNumbers 
+
 [<EntryPoint>]
 let main argv =
-    let fibCount = 20
-    let fibNumbers = calcFibNumber fibCount
-    printf "First %i numbers are: \n" fibCount
-    Seq.iter (fun i -> printf "%i " i) fibNumbers 
+    printFibSeq 20
     Console.ReadKey() |> ignore
     0 // return an integer exit code
