f364278c4bbe89b9c148d41db17fda091b6b8a8f
Morten Zdrenka Christensen <mortenzc42@gmail.com>
2020-04-14 11:32:39 +0200

    Add contents of local play-project to git repo.

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let fibOperation (grandParent, parent) items =
+    (parent, grandParent + parent)
+
+let calcFibNumber n =
+    Seq.init n id 
+    |> Seq.scan (fibOperation) (0, 1) 
+    |> Seq.map snd
+
+[<EntryPoint>]
+let main argv =
+    let fibCount = 20
+    let fibNumbers = calcFibNumber fibCount
+    printf "First %i numbers are: \n" fibCount
+    Seq.iter (fun i -> printf "%i " i) fibNumbers 
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
